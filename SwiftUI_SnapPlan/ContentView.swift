58cff805a228aef5c0497493d9dd0a4e5fc5857e
opfic <opficdev@gmail.com>
2024-12-30 17:19:54 +0900

    Initial Commit

---
+++ SwiftUI_SnapPlan/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI_SnapPlan
+//
+//  Created by opfic on 12/30/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI_SnapPlan/SwiftUI_SnapPlanApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_SnapPlanApp.swift
+//  SwiftUI_SnapPlan
+//
+//  Created by opfic on 12/30/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_SnapPlanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
