3ede9e0157e42da7b5b37c503f19574306bf5452
opfic <opficdev@gmail.com>
2025-01-04 21:33:59 +0900

    LoginView 추가

--- SnapPlan/View/LoginView.swift
+++ SnapPlan/View/LoginView.swift
@@ -8,11 +8,46 @@
 import SwiftUI
 
 struct LoginView: View {
+    @EnvironmentObject private var viewModel: LoginViewModel
+    @Environment(\.colorScheme) var colorScheme
+    
+    let screenWidth = UIScreen.main.bounds.width
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.loginView.ignoresSafeArea()
+            VStack {
+                Spacer()
+                Group {
+                    if colorScheme == .light {
+                        Image("ios_light_sq_SI@4x")
+                            .resizable()
+                    }
+                    else {
+                        Image("ios_dark_sq_SI@4x")
+                            .resizable()
+                    }
+                }
+                .scaledToFit()
+                .frame(width: screenWidth / 2)
+                .onTapGesture {
+                    Task {
+                        await viewModel.signInGoogle()
+                    }
+                }
+                
+                Spacer()
+                Text("로그인하면 약관과 개인정보 보호정책을 확인하고 동의하는 것으로 간주됩니다")
+                    .font(.caption2)
+                    .foregroundStyle(Color.gray)
+                    .multilineTextAlignment(.center)
+                    .padding(.bottom)
+            }
+        }
     }
 }
 
 #Preview {
     LoginView()
+        .environmentObject(LoginViewModel())
 }
