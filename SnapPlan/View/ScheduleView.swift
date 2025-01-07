1831d99f5fe42e447b04ab6f98760693b1135a0c
opfic <opficdev@gmail.com>
2025-01-07 17:29:11 +0900

    ScheduleView 베이스 UI 구현 중

--- SnapPlan/View/ScheduleView.swift
+++ SnapPlan/View/ScheduleView.swift
@@ -10,12 +10,52 @@ import SwiftUI
 struct ScheduleView: View {
     @EnvironmentObject private var viewModel: PlannerViewModel
     @Environment(\.colorScheme) var colorScheme
-    
     let screenWidth = UIScreen.main.bounds.width
+        
+    @State private var is12TimeFmt = true  //  후에 firebase에 저장 및 가져와야함
+    @State private var timeZoneSize = CGSize()
+    @State private var gap: CGFloat = UIScreen.main.bounds.width / 24    //  이거 조절해서 간격 조절
     
     var body: some View {
-        ScrollView(showsIndicators: false) {
-            
+        ZStack(alignment: .topLeading) {
+            VStack(spacing: 0) {
+                ScrollView(showsIndicators: false) {
+                    HStack(spacing: 0) {
+                        VStack(alignment: .trailing, spacing: gap) {
+                            ForEach(viewModel.getHours(is12hoursFmt: is12TimeFmt), id: \.self) { hour in
+                                Text(hour)
+                                    .font(.callout)
+                                    .frame(width: screenWidth / 6)
+                                    .background(
+                                        GeometryReader { geometry in
+                                            Color.white.onAppear {
+                                                timeZoneSize = geometry.size
+                                            }
+                                        }
+                                    )
+                            }
+                        }
+                        
+                        ScrollView(.horizontal, showsIndicators: false) {
+                            HStack(spacing: 0) {
+                                ForEach(viewModel.getWeekDates(date: viewModel.currentDate), id: \.self) { day in
+                                    VStack(spacing: gap) {
+                                        ForEach(viewModel.getHours(is12hoursFmt: is12TimeFmt), id: \.self) { hour in
+                                            ZStack {
+                                                Rectangle()
+                                                    .frame(height: 1)
+                                            }
+                                            .frame(height: timeZoneSize.height)
+                                        }
+                                    }
+                                }
+                                .border(Color.black)
+                                .frame(width: screenWidth - timeZoneSize.width)
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
