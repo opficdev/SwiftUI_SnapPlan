2b659917f944367311f7e7acdf470f4c16766e89
opfic <opficdev@gmail.com>
2025-01-09 21:28:11 +0900

    테스트용 코드에서 실제 컴포넌트로 사용할 수 있도록 수정

--- SnapPlan/Component/CalendarGrid.swift
+++ SnapPlan/Component/CalendarGrid.swift
@@ -10,11 +10,18 @@ import SwiftUI
 struct CalendarGrid: View {
     @EnvironmentObject var viewModel: PlannerViewModel
     @Binding var wasPast: Bool  //  이전 날짜인지 확인
+    @State private var monthData: [Date]
+    let screenWidth = UIScreen.main.bounds.width
     let columns = Array(repeating: GridItem(.flexible()), count: 7)
     
+    init(wasPast: Binding<Bool>, monthData: [Date]) {
+        self._monthData = State(initialValue: monthData)
+        self._wasPast = wasPast
+    }
+        
     var body: some View {
         LazyVGrid(columns: columns, spacing: 0) {
-            ForEach(viewModel.calendarData[1], id: \.self) { date in
+            ForEach(monthData, id: \.self) { date in
                 CalendarCell(date: date, wasPast: $wasPast)
                     .environmentObject(viewModel)
             }
@@ -24,7 +31,8 @@ struct CalendarGrid: View {
 
 #Preview {
     CalendarGrid(
-        wasPast: .constant(false)
+        wasPast: .constant(false),
+        monthData: PlannerViewModel().calendarData[1]
     )
     .environmentObject(PlannerViewModel())
 }
