2fe697e9d243a4f56487b42382326c38e84be300
opfic <opficdev@gmail.com>
2025-01-12 00:02:51 +0900

    ScrollView(.horizontal) 을 TabView로 교체

--- SnapPlan/View/ScheduleView.swift
+++ SnapPlan/View/ScheduleView.swift
@@ -15,7 +15,7 @@ struct ScheduleView: View {
     @State private var is12TimeFmt = true  //  후에 firebase에 저장 및 가져와야함
     @State private var timeZoneSize = CGSizeZero
     @State private var gap: CGFloat = UIScreen.main.bounds.width / 24    //  이거 조절해서 간격 조절
-    @State private var scrollOffset = CGFloat.zero
+    @State private var selection = 0
     
     var body: some View {
         ZStack {
@@ -29,34 +29,30 @@ struct ScheduleView: View {
                         .onTapGesture {
                             is12TimeFmt.toggle()
                         }
-                    ScrollViewReader { proxy in
-                        ScrollView(.horizontal, showsIndicators: false) {
-                            HStack(spacing: 0) {
-                                ForEach(Array(zip(calendarData.indices, calendarData)), id: \.0) { idx, date in
-                                    HStack {
-                                        Text(viewModel.dateString(date: date, component: .day))
-                                            .font(.callout)
-                                        ZStack {
-                                            RoundedRectangle(cornerRadius: 8)
-                                                .fill(
-                                                    viewModel.isSameDate(date1: date, date2: viewModel.today, components: [.year, .month, .day]) ? Color.pink : Color.gray.opacity(0.5)
-                                                )
-                                                .frame(width: screenWidth / 14, height: screenWidth / 14)
-                                            Text("\(DateFormatter.krWeekDay.string(from: date))")
-                                                .font(.callout)
-                                                .fontWeight(.bold)
-                                                .foregroundColor(Color.white)
-                                        }
-                                    }
-                                    .id(idx)
+                    
+                    TabView(selection: $selection) {
+                        ForEach(Array(zip(calendarData.indices, calendarData)), id: \.1) { idx, date in
+                            HStack {
+                                Text(viewModel.dateString(date: date, component: .day))
+                                    .font(.callout)
+                                ZStack {
+                                    RoundedRectangle(cornerRadius: 8)
+                                        .fill(
+                                            viewModel.isSameDate(date1: date, date2: viewModel.today, components: [.year, .month, .day]) ? Color.pink : Color.gray.opacity(0.5)
+                                        )
+                                        .frame(width: screenWidth / 14, height: screenWidth / 14)
+                                    Text("\(DateFormatter.krWeekDay.string(from: date))")
+                                        .font(.callout)
+                                        .fontWeight(.bold)
+                                        .foregroundColor(Color.white)
                                 }
-                                .frame(width: screenWidth - timeZoneSize.width, height: screenWidth / 10)
                             }
-                            .offset(x: -scrollOffset)
+                            .id(idx)
                         }
                         .frame(width: screenWidth - timeZoneSize.width, height: screenWidth / 10)
-                        .disabled(true)
                     }
+                    .frame(width: screenWidth - timeZoneSize.width, height: screenWidth / 10)
+                    .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
                 }
                 .background(Color.calendarBackground)
                 
@@ -85,41 +81,30 @@ struct ScheduleView: View {
                             }
                         }
                         .border(Color.gray.opacity(0.5))
-                        
-                        ScrollViewReader { proxy in
-                            ScrollView(.horizontal, showsIndicators: false) {
-                                HStack(spacing: 0) {
-                                    ForEach(Array(zip(calendarData.indices, calendarData)), id: \.0) { idx, date in
-                                        VStack(spacing: gap) {
-                                            ForEach(1...24, id: \.self) { index in
-                                                ZStack {
-                                                    Rectangle()
-                                                        .frame(height: 1)
-                                                        .foregroundColor(Color.gray.opacity(0.5))
-                                                }
-                                                .frame(height: timeZoneSize.height)
-                                            }
+                           
+                        TabView(selection: $selection) {
+                            ForEach(Array(zip(calendarData.indices, calendarData)), id: \.1) { idx, date in
+                                VStack(spacing: gap) {
+                                    ForEach(1...24, id: \.self) { index in
+                                        ZStack {
+                                            Rectangle()
+                                                .frame(height: 1)
+                                                .foregroundColor(Color.gray.opacity(0.5))
                                         }
-                                        .id(idx)
+                                        .frame(height: timeZoneSize.height)
                                     }
-                                    .border(Color.gray.opacity(0.5))
-                                    .frame(width: screenWidth - timeZoneSize.width)
                                 }
-                                .background(GeometryReader {
-                                    Color.clear.preference(key: SyncScrollViewKey.self,
-                                                           value: -$0.frame(in: .named("timeScroll")).origin.x)
-                                })
-                                .onPreferenceChange(SyncScrollViewKey.self) { value in
-                                    scrollOffset = value
-                                }
+                                .tag(idx)
                             }
-                            .onAppear {
-                                let index = calendarData.firstIndex(where: { viewModel.isSameDate(date1: $0, date2: viewModel.selectDate, components: [.year, .month, .day]) })!
-                                proxy.scrollTo(index, anchor: .center)
-                            }
-                            .coordinateSpace(name: "timeScroll")
                             .border(Color.gray.opacity(0.5))
+                            .frame(width: screenWidth - timeZoneSize.width)
+                            
                         }
+                        .onAppear {
+                            selection = calendarData.firstIndex(where: { viewModel.isSameDate(date1: $0, date2: viewModel.selectDate, components: [.year, .month, .day]) })!
+                        }
+                        .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+                        .border(Color.gray.opacity(0.5))
                     }
                 }
             }
