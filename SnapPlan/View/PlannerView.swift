e033d874b70da65d2411749ac78c0d8cbed0eb19
opfic <opficdev@gmail.com>
2025-01-26 23:43:45 +0900

    SwiftUIIntrospect 라이브러리를 사용해 수정 중

--- SnapPlan/View/PlannerView.swift
+++ SnapPlan/View/PlannerView.swift
@@ -15,103 +15,56 @@ struct PlannerView: View {
     @EnvironmentObject var loginVM: LoginViewModel
     
     var body: some View {
-        UIKitSheetWrapper()
-            .edgesIgnoringSafeArea(.all)
-            .environmentObject(plannerVM)
-            .environmentObject(loginVM)
-    }
-}
-
-struct UIKitSheetWrapper: UIViewControllerRepresentable {
-    @EnvironmentObject var plannerVM: PlannerViewModel
-    @EnvironmentObject var loginVM: LoginViewModel
-
-    func makeUIViewController(context: Context) -> UIViewController {
-        let rootController = UIViewController()
-        rootController.view.backgroundColor = .clear
-
-        DispatchQueue.main.async {
-            let sheetVC = MainViewController()
-            sheetVC.plannerVM = plannerVM
-            sheetVC.loginVM = loginVM
-            sheetVC.modalPresentationStyle = .overFullScreen
-            sheetVC.modalTransitionStyle = .crossDissolve
-            rootController.present(sheetVC, animated: true)
-        }
-        return rootController
-    }
-
-    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
-}
-
-class BackgroundViewController: UIHostingController<AnyView> {
-    init(plannerVM: PlannerViewModel, loginVM: LoginViewModel) {
-        let rootView = VStack(spacing: 0) {
+        VStack(spacing: 0) {
             CalendarView()
                 .environmentObject(plannerVM)
                 .environmentObject(loginVM)
             TimeLineView()
                 .environmentObject(plannerVM)
         }
-        super.init(rootView: AnyView(rootView))
-    }
-
-    @objc required dynamic init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-}
-
-// 메인 뷰 컨트롤러
-class MainViewController: UIViewController {
-    var plannerVM: PlannerViewModel!
-    var loginVM: LoginViewModel!
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .clear // ✅ 배경 투명 유지
-
-        // 배경 뷰에 SwiftUI 뷰 추가
-        let backgroundVC = BackgroundViewController(plannerVM: plannerVM, loginVM: loginVM)
-        addChild(backgroundVC)
-        view.addSubview(backgroundVC.view)
-        backgroundVC.view.frame = view.bounds
-        backgroundVC.didMove(toParent: self)
-    }
-
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        presentSheet()
-    }
-
-    func presentSheet() {
-        let scheduleVC = ScheduleViewController()
-        if let sheet = scheduleVC.sheetPresentationController {
-            sheet.detents = [
-                .custom(resolver: { context in 0.1 * context.maximumDetentValue }),
-                .custom(resolver: { context in 0.4 * context.maximumDetentValue }),
-                .custom(resolver: { context in 0.99 * context.maximumDetentValue })
-            ]
-            sheet.prefersGrabberVisible = true // ✅ .presentationDragIndicator(.visible)
-            sheet.prefersScrollingExpandsWhenScrolledToEdge = false
-            sheet.prefersEdgeAttachedInCompactHeight = false
-
-            // 배경 어둡게 하지 않기
-            sheet.largestUndimmedDetentIdentifier = sheet.detents[1].identifier
+        .sheet(isPresented: .constant(true)) {
+            ScheduleView(schedules: .constant(nil))
+                .presentationDetents([.fraction(0.1), .fraction(0.4), .fraction(0.99)])
+                .presentationDragIndicator(.visible)
+                .interactiveDismissDisabled(true)
+                .applyBackgroundInteraction() // ✅ iOS 버전에 따라 다른 동작 적용
         }
-
-        // 시트의 뷰가 터치 이벤트를 가로채지 않도록 설정
-        scheduleVC.view.isUserInteractionEnabled = false
-        present(scheduleVC, animated: true)
     }
 }
 
-class ScheduleViewController: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .systemBackground
-        isModalInPresentation = true // ✅ SwiftUI의 .interactiveDismissDisabled(true)와 동일
+// iOS 버전에 따라 다른 동작을 적용하는 확장
+extension View {
+    @ViewBuilder
+    func applyBackgroundInteraction() -> some View {
+        if #available(iOS 17, *) {
+            // iOS 17 이상: SwiftUI의 `.presentationBackgroundInteraction(.enabled)` 사용
+            self.presentationBackgroundInteraction(.enabled)
+        } else {
+            // iOS 16: `introspect`를 사용하여 UIKit로 제어
+            self.introspect(.sheet, on: .iOS(.v16)) { controller in
+                if let sheet = controller as? UISheetPresentationController {
+                // 커스텀 detent 생성 (0.1, 0.4, 0.99 크기)
+                    let smallDetent = UISheetPresentationController.Detent.custom { context in
+                        return context.maximumDetentValue * 0.1
+                    }
+                    let mediumDetent = UISheetPresentationController.Detent.custom { context in
+                        return context.maximumDetentValue * 0.4
+                    }
+                    let largeDetent = UISheetPresentationController.Detent.custom { context in
+                        return context.maximumDetentValue * 0.99
+                    }
+
+                    // detents 배열에 커스텀 detent 추가
+                    sheet.detents = [smallDetent, mediumDetent, largeDetent]
+
+                    // largestUndimmedDetentIdentifier를 마지막 detent로 설정 (0.99)
+                    sheet.largestUndimmedDetentIdentifier = largeDetent.identifier
+                }
+            }
+        }
     }
 }
+
 
 #Preview {
     PlannerView()
