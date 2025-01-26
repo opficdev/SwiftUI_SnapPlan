046a4ca833bff5c504c047df687a98dcc553231a
opfic <opficdev@gmail.com>
2025-01-26 23:20:40 +0900

    PlannerView 내부 뷰를 통으로 UIKit으로 재구성

--- SnapPlan/View/PlannerView.swift
+++ SnapPlan/View/PlannerView.swift
@@ -15,34 +15,25 @@ struct PlannerView: View {
     @EnvironmentObject var loginVM: LoginViewModel
     
     var body: some View {
-        ZStack(alignment: .bottomTrailing) {
-            VStack(spacing: 0) {
-                CalendarView()
-                    .environmentObject(plannerVM)
-                    .environmentObject(loginVM)
-                TimeLineView()
-                    .environmentObject(plannerVM)
-            }
-//                    .sheet(isPresented: .constant(true)) {
-//                        ScheduleView(schedules: .constant(nil))
-//                            .presentationDetents([.fraction(0.1), .fraction(0.4), .fraction(0.99)])
-//                            .presentationDragIndicator(.visible)
-//                            .interactiveDismissDisabled(true)
-//                            .presentationBackgroundInteraction(.enabled)
-//                    }
-            
-            UIKitSheetWrapper()
-        }
+        UIKitSheetWrapper()
+            .edgesIgnoringSafeArea(.all)
+            .environmentObject(plannerVM)
+            .environmentObject(loginVM)
     }
 }
 
 struct UIKitSheetWrapper: UIViewControllerRepresentable {
+    @EnvironmentObject var plannerVM: PlannerViewModel
+    @EnvironmentObject var loginVM: LoginViewModel
+
     func makeUIViewController(context: Context) -> UIViewController {
         let rootController = UIViewController()
         rootController.view.backgroundColor = .clear
 
         DispatchQueue.main.async {
             let sheetVC = MainViewController()
+            sheetVC.plannerVM = plannerVM
+            sheetVC.loginVM = loginVM
             sheetVC.modalPresentationStyle = .overFullScreen
             sheetVC.modalTransitionStyle = .crossDissolve
             rootController.present(sheetVC, animated: true)
@@ -53,10 +44,38 @@ struct UIKitSheetWrapper: UIViewControllerRepresentable {
     func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
 }
 
+class BackgroundViewController: UIHostingController<AnyView> {
+    init(plannerVM: PlannerViewModel, loginVM: LoginViewModel) {
+        let rootView = VStack(spacing: 0) {
+            CalendarView()
+                .environmentObject(plannerVM)
+                .environmentObject(loginVM)
+            TimeLineView()
+                .environmentObject(plannerVM)
+        }
+        super.init(rootView: AnyView(rootView))
+    }
+
+    @objc required dynamic init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// 메인 뷰 컨트롤러
 class MainViewController: UIViewController {
+    var plannerVM: PlannerViewModel!
+    var loginVM: LoginViewModel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .clear // ✅ 배경 투명 유지
+
+        // 배경 뷰에 SwiftUI 뷰 추가
+        let backgroundVC = BackgroundViewController(plannerVM: plannerVM, loginVM: loginVM)
+        addChild(backgroundVC)
+        view.addSubview(backgroundVC.view)
+        backgroundVC.view.frame = view.bounds
+        backgroundVC.didMove(toParent: self)
     }
 
     override func viewDidAppear(_ animated: Bool) {
@@ -75,6 +94,9 @@ class MainViewController: UIViewController {
             sheet.prefersGrabberVisible = true // ✅ .presentationDragIndicator(.visible)
             sheet.prefersScrollingExpandsWhenScrolledToEdge = false
             sheet.prefersEdgeAttachedInCompactHeight = false
+
+            // 배경 어둡게 하지 않기
+            sheet.largestUndimmedDetentIdentifier = sheet.detents[1].identifier
         }
 
         // 시트의 뷰가 터치 이벤트를 가로채지 않도록 설정
