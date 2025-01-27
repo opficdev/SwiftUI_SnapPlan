6d9daeeacb37b3b65ea34f78527e102e48faef54
opfic <opficdev@gmail.com>
2025-01-27 19:36:04 +0900

    스케줄 배경 색을 저장하는 변수 추가

--- SnapPlan/Model/TimeData.swift
+++ SnapPlan/Model/TimeData.swift
@@ -11,10 +11,16 @@ struct TimeData: Identifiable {
     let id: UUID
     let time: String
     let timePeriod: String
+    var color: Int
     
     init(time: String, timePeriod: String = "") {
         self.id = UUID()
         self.time = time
         self.timePeriod = timePeriod
+        self.color = 4
+    }
+    
+    mutating func changeColor(newColor: Int) {
+        self.color = newColor
     }
 }
