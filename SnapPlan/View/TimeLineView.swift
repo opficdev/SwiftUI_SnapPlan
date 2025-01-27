616a8c785e7fd9484d2d9aedfb62de120a324363
opfic <opficdev@gmail.com>
2025-01-27 15:29:21 +0900

    스크롤을 위아래 끝으로 드래그 해도 경계선이 끊기는 현상 수정

--- SnapPlan/View/TimeLineView.swift
+++ SnapPlan/View/TimeLineView.swift
@@ -40,7 +40,10 @@ struct TimeLineView: View {
                                 ZStack {
                                     RoundedRectangle(cornerRadius: 8)
                                         .fill(
-                                            viewModel.isSameDate(date1: date, date2: viewModel.today, components: [.year, .month, .day]) ? Color.timeBar : Color.gray.opacity(0.5)
+                                            viewModel.isSameDate(
+                                                date1: date,
+                                                date2: viewModel.today,
+                                                components: [.year, .month, .day]) ? Color.timeBar : Color.gray.opacity(0.5)
                                         )
                                         .frame(width: screenWidth / 14, height: screenWidth / 14)
                                     Text("\(DateFormatter.krWeekDay.string(from: date))")
@@ -59,83 +62,78 @@ struct TimeLineView: View {
                 .background(Color.calendar)
                 
                 ScrollViewReader { proxy in
-                    ScrollView(showsIndicators: false) {
-                        HStack(spacing: 0) {
-                            ZStack(alignment: .topTrailing) {
-                                VStack(alignment: .trailing, spacing: gap) {
-                                    let hours = viewModel.getHours(is12hoursFmt: is12TimeFmt)
-                                    ForEach(Array(zip(hours.indices, hours)), id: \.1.id) { index, hour in
-                                        Text("\(hour.timePeriod) \(hour.time)")
-                                            .font(.caption)
-                                            .foregroundStyle(Color.gray)
-                                            .opacity(
-                                                viewModel.isCollapsed(timeZoneHeight: timeZoneSize.height, gap: gap, index: index) ? 0 : 1
-                                            )
-                                            .padding(.trailing, 2)
-                                            .frame(width: screenWidth / 7, alignment: .trailing)
-                                            .background(
-                                                GeometryReader { geometry in
-                                                    Color.clear.onAppear {
-                                                        if timeZoneSize == CGSizeZero {
-                                                            timeZoneSize = geometry.size
+                    ZStack(alignment: .leading) {
+                        ScrollView(showsIndicators: false) {
+                            HStack(spacing: 0) {
+                                ZStack(alignment: .topTrailing) {
+                                    VStack(alignment: .trailing, spacing: gap) {
+                                        let hours = viewModel.getHours(is12hoursFmt: is12TimeFmt)
+                                        ForEach(Array(zip(hours.indices, hours)), id: \.1.id) { index, hour in
+                                            Text("\(hour.timePeriod) \(hour.time)")
+                                                .font(.caption)
+                                                .foregroundStyle(Color.gray)
+                                                .opacity(
+                                                    viewModel.isCollapsed(
+                                                        timeZoneHeight: timeZoneSize.height,
+                                                        gap: gap, index: index) ? 0 : 1
+                                                )
+                                                .padding(.trailing, 2)
+                                                .frame(width: screenWidth / 7, alignment: .trailing)
+                                                .background(
+                                                    GeometryReader { geometry in
+                                                        Color.clear.onAppear {
+                                                            if timeZoneSize == CGSizeZero {
+                                                                timeZoneSize = geometry.size
+                                                            }
                                                         }
                                                     }
-                                                }
-                                            )
-                                    }
-                                }
-                                .background(
-                                    HStack {
-                                        Spacer()
-                                        Rectangle()
-                                            .frame(width: 1)
-                                            .foregroundStyle(Color.gray.opacity(0.5))
-                                    }
-                                )
-                                Text(viewModel.getHoursAndMiniute(is12hoursFmt: is12TimeFmt))
-                                    .font(.caption)
-                                    .padding(.trailing, 2)
-                                    .offset(y: viewModel.getOffsetFromMiniute(
-                                        for: viewModel.today,
-                                        timeZoneHeight: timeZoneSize.height,
-                                        gap: gap)
-                                    )
-                            }
-                            
-                            //  좌우로 드래그 가능한 TimeLine
-                            TabView(selection: $selection) {
-                                ForEach(Array(zip(calendarData.indices, calendarData)), id: \.1) { idx, date in
-                                    ZStack(alignment: .top) {
-                                        VStack(spacing: gap) {
-                                            ForEach(0...24, id: \.self) { index in
-                                                VStack {
-                                                    Spacer()
-                                                        .onTapGesture {
-                                                            
-                                                        }
-                                                    Rectangle()
-                                                        .frame(height: 1)
-                                                        .foregroundColor(Color.gray.opacity(0.5))
-                                                    Spacer()
-                                                        .onTapGesture {
-                                                            
-                                                        }
-                                                }
-                                                .frame(height: timeZoneSize.height)
-                                            }
+                                                )
                                         }
-                                        
-                                        //  스케줄 목록을 표시하는 ScheduleBox
-                                        ForEach(Array(zip(schedules.indices, schedules)), id: \.1.id) { idx, schedule in
-                                            let (startOffset, boxHeight) = viewModel.getTimeBoxOffset(
-                                                from: schedule,
-                                                timeZoneHeight: timeZoneSize.height,
-                                                gap: gap
-                                            )
-                                            ScheduleBox(
-                                                height: boxHeight,
-                                                isChanging: $schedules[idx].isChanging
-                                            )
+                                    }
+                                    Text(viewModel.getHoursAndMiniute(is12hoursFmt: is12TimeFmt))
+                                        .font(.caption)
+                                        .padding(.trailing, 2)
+                                        .offset(y: viewModel.getOffsetFromMiniute(
+                                            for: viewModel.today,
+                                            timeZoneHeight: timeZoneSize.height,
+                                            gap: gap)
+                                        )
+                                }
+                                
+                                //  좌우로 드래그 가능한 TimeLine
+                                TabView(selection: $selection) {
+                                    ForEach(Array(zip(calendarData.indices, calendarData)), id: \.1) { idx, date in
+                                        ZStack(alignment: .top) {
+                                            VStack(spacing: gap) {
+                                                ForEach(0...24, id: \.self) { index in
+                                                    VStack {
+                                                        Spacer()
+                                                            .onTapGesture {
+                                                                
+                                                            }
+                                                        Rectangle()
+                                                            .frame(height: 1)
+                                                            .foregroundColor(Color.gray.opacity(0.5))
+                                                        Spacer()
+                                                            .onTapGesture {
+                                                                
+                                                            }
+                                                    }
+                                                    .frame(height: timeZoneSize.height)
+                                                }
+                                            }
+                                            
+                                            //  스케줄 목록을 표시하는 ScheduleBox
+                                            ForEach(Array(zip(schedules.indices, schedules)), id: \.1.id) { idx, schedule in
+                                                let (startOffset, boxHeight) = viewModel.getTimeBoxOffset(
+                                                    from: schedule,
+                                                    timeZoneHeight: timeZoneSize.height,
+                                                    gap: gap
+                                                )
+                                                ScheduleBox(
+                                                    height: boxHeight,
+                                                    isChanging: $schedules[idx].isChanging
+                                                )
                                                 .offset(y: timeZoneSize.height + startOffset)
                                                 .onTapGesture {
                                                     if schedules[idx].isChanging {
@@ -146,54 +144,70 @@ struct TimeLineView: View {
                                                         schedules[idx].isChanging = true
                                                     }
                                                 }
+                                            }
+                                            
+                                            //  현 시간 표시하는 TimeBar
+                                            TimeBar(
+                                                height: timeZoneSize.height,
+                                                showVerticalLine: viewModel.isSameDate(
+                                                    date1: date,
+                                                    date2: viewModel.today,
+                                                    components: [.year, .month, .day])
+                                            )
+                                            .id(UUID())
+                                            .padding(
+                                                .leading, viewModel.isSameDate(
+                                                    date1: date,
+                                                    date2: viewModel.today,
+                                                    components: [.year, .month, .day]) ? 2 : 0
+                                            )
+                                            .offset(y: viewModel.getOffsetFromMiniute(
+                                                for: viewModel.today,
+                                                timeZoneHeight: timeZoneSize.height,
+                                                gap: gap)
+                                            )
                                         }
-         
-                                        //  현 시간 표시하는 TimeBar
-                                        TimeBar(
-                                            height: timeZoneSize.height,
-                                            showVerticalLine: viewModel.isSameDate(date1: date, date2: viewModel.today, components: [.year, .month, .day])
-                                        )
-                                        .id(UUID())
-                                        .padding(
-                                            .leading, viewModel.isSameDate(date1: date, date2: viewModel.today, components: [.year, .month, .day]) ? 2 : 0
-                                        )
-                                        .offset(y: viewModel.getOffsetFromMiniute(
-                                            for: viewModel.today,
-                                            timeZoneHeight: timeZoneSize.height,
-                                            gap: gap)
-                                        )
+                                        .tag(idx)
                                     }
-                                    .tag(idx)
+                                    .frame(width: screenWidth - timeZoneSize.width)
+                                    .background(
+                                        HStack {
+                                            Rectangle()
+                                                .frame(width: 1)
+                                                .foregroundStyle(Color.gray.opacity(0.5))
+                                            Spacer()
+                                        }
+                                    )
                                 }
-                                .frame(width: screenWidth - timeZoneSize.width)
-                                .background(
-                                    HStack {
-                                        Rectangle()
-                                            .frame(width: 1)
-                                            .foregroundStyle(Color.gray.opacity(0.5))
-                                        Spacer()
-                                    }
+                                .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+                                .simultaneousGesture(
+                                    MagnificationGesture()
+                                        .onChanged { value in   // min: 너무 커지지 않게, max: 너무 작아지지 않게
+                                            gap = min(screenWidth, max(lastGap * value, screenWidth / 24))
+                                        }
+                                        .onEnded { _ in
+                                            lastGap = max(gap, screenWidth / 24)
+                                        }
                                 )
                             }
-                            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
-                            .simultaneousGesture(
-                                MagnificationGesture()
-                                    .onChanged { value in   // min: 너무 커지지 않게, max: 너무 작아지지 않게
-                                        gap = min(screenWidth, max(lastGap * value, screenWidth / 24))
-                                    }
-                                    .onEnded { _ in
-                                        lastGap = max(gap, screenWidth / 24)
-                                    }
-                            )
+                            .id(0)
                         }
-                        .id(0)
+                        HStack {
+                            Rectangle()
+                                .frame(width: 1)
+                                .foregroundStyle(Color.gray.opacity(0.5))
+                                .offset(x: timeZoneSize.width)
+                        }
                     }
                 }
             }
             .onAppear {
                 calendarData = viewModel.calendarData[1]
                 selection = calendarData.firstIndex(where: {
-                   viewModel.isSameDate(date1: $0, date2: viewModel.selectDate, components: [.year, .month, .day]) }
+                   viewModel.isSameDate(
+                    date1: $0,
+                    date2: viewModel.selectDate,
+                    components: [.year, .month, .day]) }
                 )!
             }
             .onChange(of: selection) { value in
@@ -208,7 +222,10 @@ struct TimeLineView: View {
                         calendarData = viewModel.calendarDates(date: value)
                     }
                     selection = calendarData.firstIndex(where: {
-                        viewModel.isSameDate(date1: $0, date2: value, components: [.year, .month, .day]) }
+                        viewModel.isSameDate(
+                            date1: $0,
+                            date2: value,
+                            components: [.year, .month, .day]) }
                     )!
                 }
             }
