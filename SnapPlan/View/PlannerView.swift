dd30f3b3509fbe3e4856e13326ed64f275bec47d
opfic <opficdev@gmail.com>
2025-01-27 11:02:48 +0900

    SwiftUIIntrospect로 기능 통합

--- SnapPlan/View/PlannerView.swift
+++ SnapPlan/View/PlannerView.swift
@@ -23,35 +23,23 @@ struct PlannerView: View {
                 .environmentObject(plannerVM)
         }
         .sheet(isPresented: .constant(true)) {
-            ScheduleView(schedules: .constant(nil))
-                .presentationDetents([.fraction(0.1), .fraction(0.4), .fraction(0.99)])
+            ScheduleView(schedule: .constant(nil))
+                .presentationDetents(
+                    firebaseVM.isScheduleExist() ?
+                    [.fraction(0.4), .fraction(0.99)] : [.fraction(0.1)]
+                )
                 .presentationDragIndicator(.visible)
                 .interactiveDismissDisabled(true)
-                .applyBackgroundInteraction() // iOS 버전에 따라 다른 동작 적용
-        }
-    }
-}
-
-// iOS 버전에 따라 다른 동작을 적용하는 확장
-extension View {
-    @ViewBuilder
-    func applyBackgroundInteraction() -> some View {
-        if #available(iOS 17, *) {
-            // iOS 17 이상: SwiftUI의 `.presentationBackgroundInteraction(.enabled)` 사용
-            self.presentationBackgroundInteraction(.enabled)
-        } else {
-            // iOS 16: `introspect`를 사용하여 UIKit로 제어
-            self.introspect(.sheet, on: .iOS(.v16)) { controller in
-                if let sheet = controller as? UISheetPresentationController {
-                    if let maxDetent = sheet.detents.max(by: { $0.identifier.rawValue < $1.identifier.rawValue }) {
-                        sheet.largestUndimmedDetentIdentifier = maxDetent.identifier
+                .introspect(.sheet, on: .iOS(.v16, .v17, .v18)) { controller in
+                    if let sheet = controller as? UISheetPresentationController {
+                        if let maxDetent = sheet.detents.max(by: { $0.identifier.rawValue < $1.identifier.rawValue }) {
+                            sheet.largestUndimmedDetentIdentifier = maxDetent.identifier
+                        }
                     }
                 }
-            }
         }
     }
 }
-
 
 #Preview {
     PlannerView()
