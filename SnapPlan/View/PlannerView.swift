582afc3c19ff34161cc8e64b8a10dca94fb14ec8
opfic <opficdev@gmail.com>
2025-01-29 21:52:20 +0900

    presentationDetents() 매개변수 함수화

--- SnapPlan/View/PlannerView.swift
+++ SnapPlan/View/PlannerView.swift
@@ -14,6 +14,8 @@ struct PlannerView: View {
     @StateObject var firebaseVM = FirebaseViewModel()
     @EnvironmentObject var loginVM: LoginViewModel
     @State private var showSideBar = false
+    @State private var didSelectSchedule = false
+    @State private var tapButton = false
     
     var body: some View {
         VStack(spacing: 0) {
@@ -21,31 +23,38 @@ struct PlannerView: View {
                 showSideBar: $showSideBar
             )
                 .environmentObject(plannerVM)
-            TimeLineView()
+            TimeLineView(
+                didSelectSchedule: $didSelectSchedule
+            )
                 .environmentObject(plannerVM)
         }
         .sheet(isPresented: .constant(true)) {
-            ScheduleView(schedule: .constant(nil))
-//                .presentationDetents(
-//                    [.fraction(0.4), .fraction(0.99)] : [.fraction(0.07)]
-//                )
-                .presentationDragIndicator(.visible)
-                .interactiveDismissDisabled(true)
-                .introspect(.sheet, on: .iOS(.v16, .v17, .v18)) { controller in
-                    if let sheet = controller as? UISheetPresentationController {
-                        if let maxDetent = sheet.detents.max(by: { $0.identifier.rawValue < $1.identifier.rawValue }) {
-                            sheet.largestUndimmedDetentIdentifier = maxDetent.identifier
-                        }
+            ScheduleView(
+                schedule: .constant(nil),
+                tapButton: $tapButton
+            )
+            .ignoresSafeArea(.keyboard)
+            .presentationDetents(getDetent())
+            .presentationDragIndicator(.visible)
+            .interactiveDismissDisabled(true)
+            .introspect(.sheet, on: .iOS(.v16, .v17, .v18)) { controller in
+                if let sheet = controller as? UISheetPresentationController {
+                    if let maxDetent = sheet.detents.max(by: { $0.identifier.rawValue < $1.identifier.rawValue }) {
+                        sheet.largestUndimmedDetentIdentifier = maxDetent.identifier
                     }
                 }
+            }
         }
-        .onAppear {
-            
+    }
+    
+    private func getDetent() -> Set<PresentationDetent> {
+        if didSelectSchedule {
+            return [.fraction(0.4), .fraction(0.9)]
         }
-        .onChange(of: plannerVM.selectDate) { newDate in
-            
+        if tapButton {
+            return [.fraction(0.9)]
         }
-        
+        return [.fraction(0.07)]
     }
 }
 
