70c1006978980e53f1aec8efa2c6770bd44c3c86
opfic <opficdev@gmail.com>
2025-01-31 20:58:08 +0900

    시간을 선택하는 Picker

---
+++ SnapPlan/Component/TimePicker.swift
@@ -0,0 +1,18 @@
+//
+//  TimePicker.swift
+//  SnapPlan
+//
+//  Created by opfic on 1/31/25.
+//
+
+import SwiftUI
+
+struct TimePicker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TimePicker()
+}
