a84673b02e21fb2b5a8603613d55d940d6a0bc6d
opfic <opficdev@gmail.com>
2025-02-01 02:56:17 +0900

    .interactiveDismissDisabled() 가 항상 참이어서 외부로 이동

--- SnapPlan/View/PlannerView.swift
+++ SnapPlan/View/PlannerView.swift
@@ -26,14 +26,15 @@ struct PlannerView: View {
         }
         .sheet(isPresented: .constant(true)) {
             ScheduleView(schedule: .constant(nil))
-            .presentationDragIndicator(.visible)
-            .introspect(.sheet, on: .iOS(.v16, .v17, .v18)) { controller in //  sheet가 올라와있어도 하위 뷰에 터치가 가능하도록 해줌
-                if let sheet = controller as? UISheetPresentationController {
-                    if let maxDetent = sheet.detents.max(by: { $0.identifier.rawValue < $1.identifier.rawValue }) {
-                        sheet.largestUndimmedDetentIdentifier = maxDetent.identifier
+                .presentationDragIndicator(.visible)
+                .interactiveDismissDisabled(true)   //  사용자가 임의로 sheet를 완전히 내리는 것을 방지
+                .introspect(.sheet, on: .iOS(.v16, .v17, .v18)) { controller in //  sheet가 올라와있어도 하위 뷰에 터치가 가능하도록 해줌
+                    if let sheet = controller as? UISheetPresentationController {
+                        if let maxDetent = sheet.detents.max(by: { $0.identifier.rawValue < $1.identifier.rawValue }) {
+                            sheet.largestUndimmedDetentIdentifier = maxDetent.identifier
+                        }
                     }
                 }
-            }
         }
     }
 }
