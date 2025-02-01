67ebadd11dfba475fa8ceaf0942b2da3a9a79a06
opfic <opficdev@gmail.com>
2025-02-01 13:42:06 +0900

    동적으로 picker의 height 측정하여 반환

--- SnapPlan/Component/DateTimePicker.swift
+++ SnapPlan/Component/DateTimePicker.swift
@@ -10,37 +10,46 @@ import SwiftUI
 struct DateTimePicker: View {
     @Environment(\.dismiss) var dismiss
     @Binding var selectedTime: Date
+    @Binding var pickerHeight: CGFloat
     @State private var component: DatePickerComponents
     
-    init(selectedTime: Binding<Date>, component: DatePickerComponents) {
+    init(selectedTime: Binding<Date>, pickerHeight: Binding<CGFloat>,component: DatePickerComponents) {
         self._selectedTime = selectedTime
+        self._pickerHeight = pickerHeight
         self._component = State(initialValue: component)
     }
        
     var body: some View {
-       VStack {
-           DatePicker(
-               "",
-               selection: $selectedTime,
-               displayedComponents: component
-           )
-           .datePickerStyle(.wheel)
-           .labelsHidden()
-
-           Button(action: {
-               dismiss()
-           }) {
-               ZStack {
-                   RoundedRectangle(cornerRadius: 10)
-                          .fill(Color.macBlue)
-                          .frame(height: 35)
-                   Text("확인")
-                       .foregroundStyle(Color.white)
-               }
-           }
-       }
-       .padding()
-       .background(Color.calendar)
+        VStack {
+            DatePicker(
+                "",
+                selection: $selectedTime,
+                displayedComponents: component
+            )
+            .datePickerStyle(.wheel)
+            .labelsHidden()
+            
+            Button(action: {
+                dismiss()
+            }) {
+                ZStack {
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(Color.macBlue)
+                        .frame(height: 35)
+                    Text("확인")
+                        .foregroundStyle(Color.white)
+                }
+            }
+        }
+        .padding()
+        .background(
+            GeometryReader { geometry in
+                Color.calendar.onAppear {
+                    pickerHeight = geometry.size.height
+                    print(pickerHeight)
+                }
+            }
+        )
     }
     
     private var formattedTime: String {
@@ -53,6 +62,7 @@ struct DateTimePicker: View {
 #Preview {
     DateTimePicker(
         selectedTime: .constant(Date()),
+        pickerHeight: .constant(0),
         component: .hourAndMinute
     )
 }
