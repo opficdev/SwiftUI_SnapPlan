a88086a78e8e90140cfdd5bc6f036f346a3a132a
opfic <opficdev@gmail.com>
2025-02-05 15:41:06 +0900

    popover에서 sheet로 대체함에 따라 ui 수정

--- SnapPlan/Component/ColorSelector.swift
+++ SnapPlan/Component/ColorSelector.swift
@@ -6,130 +6,55 @@
 //
 
 import SwiftUI
-import SwiftUIIntrospect
 
-struct ColorPicker: View {
+struct ColorSelector: View {
     let colorArr = [
         Color.macBlue, Color.macPurple, Color.macPink, Color.macRed,
         Color.macOrange, Color.macYellow, Color.macGreen
     ]
+    @Environment(\.dismiss) var dismiss
+    @State private var height = CGFloat.zero
+    @State private var colorWidth: CGFloat = UIScreen.main.bounds.width / 15
     @Binding var color: Int
+    
     var body: some View {
         HStack {
             ForEach(Array(zip(colorArr.indices, colorArr)), id: \.1) { idx, color in
                 RoundedRectangle(cornerRadius: 5)
                     .fill(color)
-                    .frame(width: 20, height: 20)
+                    .frame(
+                        width: self.color == idx ? colorWidth * 1.1 : colorWidth,
+                        height: self.color == idx ? colorWidth * 1.1 : colorWidth)
                     .onTapGesture {
                         self.color = idx
+                        dismiss()
                     }
+                    .overlay(
+                        ZStack {
+                            if self.color == idx {
+                                Image(systemName: "checkmark")
+                                    .foregroundStyle(Color(UIColor.systemBackground))
+                            }
+                        }
+                    )
             }
         }
-//        .introspect(.popover, on: .iOS(.v16, .v17, .v18)) { popover in   //  UIPopoverPresentationController
-//            popover.permittedArrowDirections = []
-//        }
-    }
-}
-
-
-struct tentView: View {
-
-    enum PopoverTarget {
-        case text1
-        case text2
-        case text3
-
-        var anchorForPopover: UnitPoint {
-            switch self {
-            case .text1: .top
-            case .text2: .bottom
-            case .text3: .bottom
-            }
-        }
-    }
-
-    @State private var popoverTarget: PopoverTarget?
-    @Namespace private var nsPopover
-
-    @ViewBuilder
-    private var customPopover: some View {
-        if let popoverTarget {
-            Text("Popover for \(popoverTarget)")
-                .padding()
-                .foregroundStyle(.gray)
-                .background {
-                    RoundedRectangle(cornerRadius: 10)
-                        .fill(Color(white: 0.95))
-                        .shadow(radius: 6)
+        .padding()
+        .background(
+            GeometryReader { geometry in
+                Color.clear.onAppear {
+                    height = geometry.size.height
                 }
-                .padding()
-                .matchedGeometryEffect(
-                    id: popoverTarget,
-                    in: nsPopover,
-                    properties: .position,
-                    anchor: popoverTarget.anchorForPopover,
-                    isSource: false
-                )
-        }
-    }
-
-    private func showPopover(target: PopoverTarget) {
-        if popoverTarget != nil {
-            withAnimation {
-                popoverTarget = nil
             }
-            DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) { // 애니메이션 시간 고려
-                popoverTarget = target
-            }
-        } else {
-            popoverTarget = target
-        }
-    }
-
-    var body: some View {
-        ZStack {
-            VStack {
-                Text("Text 1")
-                    .padding()
-                    .background(.blue)
-                    .onTapGesture { showPopover(target: .text1) }
-                    .matchedGeometryEffect(id: PopoverTarget.text1, in: nsPopover, anchor: .bottom)
-                    .padding(.top, 50)
-                    .padding(.leading, 100)
-                    .frame(maxWidth: .infinity, alignment: .leading)
-
-                Text("Text 2")
-                    .padding()
-                    .background(.orange)
-                    .onTapGesture { showPopover(target: .text2) }
-                    .matchedGeometryEffect(id: PopoverTarget.text2, in: nsPopover, anchor: .topLeading)
-                    .padding(.top, 100)
-                    .padding(.trailing, 40)
-                    .frame(maxWidth: .infinity, alignment: .trailing)
-
-                Spacer()
-
-                Text("Text 3")
-                    .padding()
-                    .background(.green)
-                    .onTapGesture { showPopover(target: .text3) }
-                    .matchedGeometryEffect(id: PopoverTarget.text3, in: nsPopover, anchor: .top)
-                    .padding(.bottom, 250)
-            }
-            customPopover
-                .transition(
-                    .opacity.combined(with: .scale)
-                    .animation(.bouncy(duration: 0.25, extraBounce: 0.2))
-                )
-        }
-        .foregroundStyle(.white)
-        .contentShape(Rectangle())
-        .onTapGesture {
-            popoverTarget = nil
-        }
+        )
+        .presentationDragIndicator(.visible)
+        .presentationDetents([.height(height)])
     }
 }
+
 
 #Preview {
-    ColorPicker(color: .constant(0))
+    ColorSelector(
+        color: .constant(0)
+    )
 }
