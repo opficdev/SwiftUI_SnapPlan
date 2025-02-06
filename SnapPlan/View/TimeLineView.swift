3fd9b97613552cfb36566d80878961ec3e72f83f
opfic <opficdev@gmail.com>
2025-02-06 18:38:18 +0900

    Firebase에서 받아와야 하는 변수들은 뷰모델 변수로 변경

--- SnapPlan/View/TimeLineView.swift
+++ SnapPlan/View/TimeLineView.swift
@@ -6,35 +6,28 @@
 //
 
 import SwiftUI
-import FirebaseFirestore
 
 struct TimeLineView: View {
     @EnvironmentObject private var plannerVM: PlannerViewModel
     @EnvironmentObject private var firebaseVM: FirebaseViewModel
     @Environment(\.colorScheme) var colorScheme
-    @State private var is12TimeFmt = true
     @State private var timeZoneSize = CGSizeZero
     @State private var selection = 0
     @State private var calendarData = [Date]()
     @State private var gap = UIScreen.main.bounds.width / 24    //  이거 조절해서 간격 조절
     @State private var lastGap = UIScreen.main.bounds.width / 24
-    @State private var schedules = [ScheduleData]()
     let screenWidth = UIScreen.main.bounds.width
-    
-    init() {
-
-    }
     
     var body: some View {
         ZStack {
             Color.timeLine.ignoresSafeArea()
             VStack(alignment: .leading, spacing: 0) {
                 HStack(spacing: 0) {
-                    Text(is12TimeFmt ? "12시간제" : "24시간제")
+                    Text(firebaseVM.is12TimeFmt ? "12시간제" : "24시간제")
                         .frame(width: screenWidth / 7)
                         .font(.caption)
                         .onTapGesture {
-                            is12TimeFmt.toggle()
+                            firebaseVM.is12TimeFmt.toggle()
                         }
                     
                     HStack {
@@ -65,7 +58,7 @@ struct TimeLineView: View {
                             HStack(spacing: 0) {
                                 ZStack(alignment: .topTrailing) {
                                     VStack(alignment: .trailing, spacing: gap) {
-                                        let hours = plannerVM.getHours(is12hoursFmt: is12TimeFmt)
+                                        let hours = plannerVM.getHours(is12hoursFmt: firebaseVM.is12TimeFmt)
                                         ForEach(Array(zip(hours.indices, hours)), id: \.1.id) { index, hour in
                                             Text("\(hour.timePeriod) \(hour.time)")
                                                 .font(.caption)
@@ -93,7 +86,7 @@ struct TimeLineView: View {
                                         plannerVM.getDateString(
                                             for: plannerVM.today,
                                             components: [.hour, .minute],
-                                            is12hoursFmt: is12TimeFmt
+                                            is12hoursFmt: firebaseVM.is12TimeFmt
                                         )
                                     )
                                         .font(.caption)
@@ -128,7 +121,7 @@ struct TimeLineView: View {
                                             }
                                             
                                             //  스케줄 목록을 표시하는 ScheduleBox
-                                            ForEach(Array(zip(schedules.indices, schedules)), id: \.1.id) { idx, schedule in
+                                            ForEach(Array(zip(firebaseVM.schedules.indices, firebaseVM.schedules)), id: \.1.id) { idx, schedule in
                                                 let (startOffset, boxHeight) = plannerVM.getTimeBoxOffset(
                                                     from: schedule,
                                                     timeZoneHeight: timeZoneSize.height,
@@ -136,16 +129,16 @@ struct TimeLineView: View {
                                                 )
                                                 ScheduleBox(
                                                     height: boxHeight,
-                                                    isChanging: $schedules[idx].isChanging
+                                                    isChanging: $firebaseVM.schedules[idx].isChanging
                                                 )
                                                 .offset(y: timeZoneSize.height + startOffset)
                                                 .onTapGesture {
-                                                    if schedules[idx].isChanging {
-                                                        schedules[idx].isChanging = false
+                                                    if firebaseVM.schedules[idx].isChanging {
+                                                        firebaseVM.schedules[idx].isChanging = false
                                                     }
                                                     else {
-                                                        schedules.indices.forEach { schedules[$0].isChanging = false }
-                                                        schedules[idx].isChanging = true
+                                                        firebaseVM.schedules.indices.forEach { firebaseVM.schedules[$0].isChanging = false }
+                                                        firebaseVM.schedules[idx].isChanging = true
                                                     }
                                                     
                                                 }
@@ -237,13 +230,12 @@ struct TimeLineView: View {
                 }
             }
         }
-        .onChange(of: is12TimeFmt) { value in
-            firebaseVM.set12TimeFmt(timeFmt: value) { error in
-                if let error = error {
-                    print(error)
-                }
-                else {
-                    print("정상적으로 12시간제 재저장")
+        .onChange(of: firebaseVM.is12TimeFmt) { value in
+            Task {
+                do {
+                    try await firebaseVM.set12TimeFmt(timeFmt: value)
+                } catch {
+                    print("12시간제 변경 에러", error.localizedDescription)
                 }
             }
         }
