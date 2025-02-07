6fad8036157b4c562eed5e9ae4387e554110194f
opfic <opficdev@gmail.com>
2025-02-07 20:31:23 +0900

    화면 모드를 자동, 라이트, 다크 모드로 변환하는 뷰 추가

--- SnapPlan/View/ThemeView.swift
+++ SnapPlan/View/ThemeView.swift
@@ -8,11 +8,83 @@
 import SwiftUI
 
 struct ThemeView: View {
+    @EnvironmentObject var firebaseVM: FirebaseViewModel
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            List {
+                Button(action: {
+                    setAppTheme(.unspecified)
+                    Task {
+                        try await firebaseVM.setScreenMode(mode: .unspecified)
+                        firebaseVM.screenMode = .unspecified
+                    }
+                }) {
+                    HStack {
+                        Text("자동")
+                            .foregroundStyle(Color.primary)
+                        Spacer()
+                        if firebaseVM.screenMode == .unspecified {
+                            Image(systemName: "checkmark")
+                        }
+                    }
+                }
+                .listRowBackground(Color.timeLine)
+                Button(action: {
+                    setAppTheme(.light)
+                    Task {
+                        try await firebaseVM.setScreenMode(mode: .light)
+                        firebaseVM.screenMode = .light
+                    }
+                }) {
+                    HStack {
+                        Text("라이트 모드")
+                            .foregroundStyle(Color.primary)
+                        Spacer()
+                        if firebaseVM.screenMode == .light {
+                            Image(systemName: "checkmark")
+                        }
+                    }
+                }
+                .listRowBackground(Color.timeLine)
+                Button(action: {
+                    setAppTheme(.dark) // 다크 모드
+                    Task {
+                        try await firebaseVM.setScreenMode(mode: .dark)
+                        firebaseVM.screenMode = .dark
+                    }
+                }) {
+                    HStack {
+                        Text("다크 모드")
+                            .foregroundStyle(Color.primary)
+                        Spacer()
+                        if firebaseVM.screenMode == .dark {
+                            Image(systemName: "checkmark")
+                        }
+                    }
+                }
+                .listRowBackground(Color.timeLine)
+            }
+            .scrollContentBackground(.hidden)
+            .background(Color.calendar)
+        }
+        .toolbar {
+            ToolbarItem(placement: .principal) {
+                Text("테마")
+            }     
+        }
+    }
+    
+    private func setAppTheme(_ style: UIUserInterfaceStyle) {
+        DispatchQueue.main.async {
+            if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene {
+                windowScene.windows.forEach { window in
+                    window.overrideUserInterfaceStyle = style
+                }
+            }
+        }
     }
 }
-
 #Preview {
     ThemeView()
 }
