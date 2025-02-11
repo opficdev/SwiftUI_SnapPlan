bd5e7de167595f47726b955c9fa024390d71ca2d
opfic <opficdev@gmail.com>
2025-02-11 22:07:07 +0900

    extension으로 분리

--- SnapPlan/Model/ScheduleData.swift
+++ SnapPlan/Model/ScheduleData.swift
@@ -63,15 +63,3 @@ struct ScheduleData: Identifiable {
         case custom = "custom"
     }
 }
-
-extension ScheduleData: Equatable {
-    static func == (lhs: ScheduleData, rhs: ScheduleData) -> Bool {
-        return lhs.id == rhs.id &&
-               lhs.title == rhs.title &&
-               lhs.timeLine == rhs.timeLine &&
-               lhs.cycleOption == rhs.cycleOption &&
-               lhs.location == rhs.location &&
-               lhs.description == rhs.description &&
-               lhs.color == rhs.color
-    }
-}
