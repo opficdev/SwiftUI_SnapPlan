79a16b62191c0761f32bda85fe56858eebbcd3d2
opfic <opficdev@gmail.com>
2025-02-11 10:16:09 +0900

    toolbar 수정 및 List에 이메일 및 개인정보 처리방침 추가

--- SnapPlan/View/SettingView.swift
+++ SnapPlan/View/SettingView.swift
@@ -16,13 +16,11 @@ struct SettingView: View {
     var body: some View {
         NavigationStack {
             VStack(alignment: .trailing, spacing: 0) {
-                Button(action: {
-                    dismiss()
-                }) {
-                    Text("닫기")
-                }
-                .padding(.trailing)
                 List {
+                    Section(header: Text("계정")) {
+                        Text(firebaseVM.email)
+                    }
+                    .listRowBackground(Color.timeLine)
                     Section(header: Text("테마")) {
                         NavigationLink(destination: ThemeView()) {
                             Text("테마")
@@ -37,6 +35,11 @@ struct SettingView: View {
                             Text("버전 정보")
                             Spacer()
                             Text(appVersion)
+                        }
+                        Button(action: {
+//                            Link()    //  노션 링크
+                        }) {
+                            Text("개인정보 처리방침")
                         }
                         Button(role: .destructive, action: {
                             logoutAlert = true
@@ -65,6 +68,24 @@ struct SettingView: View {
             } message: {
                 Text("로그아웃하시겠습니까?")
             }
+            .toolbar {
+                ToolbarItem(placement: .principal) {
+                    Text("설정")
+                        .bold()
+                }
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button(action: {
+                        dismiss()
+                    }) {
+                        Text("닫기")
+                            .font(.caption)
+                    }
+                }
+            }
+            .navigationTitle(Text(""))  //  < 모양 이미지 제거
+            .toolbarBackground(Color.timeLine, for: .navigationBar)
+            .toolbarBackground(.visible, for: .navigationBar)
+            .navigationBarTitleDisplayMode(.inline)
         }
     }
 }
