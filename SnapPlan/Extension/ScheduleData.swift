507192223021e7949189418081ec54ae35cce128
opfic <opficdev@gmail.com>
2025-02-11 22:01:37 +0900

    ScheduleData의 Equatable 프로토콜을 만족하기 위한 extension

---
+++ SnapPlan/Extension/ScheduleData.swift
@@ -0,0 +1,20 @@
+//
+//  ScheduleData.swift
+//  SnapPlan
+//
+//  Created by opfic on 2/11/25.
+//
+
+import Foundation
+
+extension ScheduleData: Equatable {
+    static func == (lhs: ScheduleData, rhs: ScheduleData) -> Bool {
+        return lhs.id == rhs.id &&
+               lhs.title == rhs.title &&
+               lhs.timeLine == rhs.timeLine &&
+               lhs.cycleOption == rhs.cycleOption &&
+               lhs.location == rhs.location &&
+               lhs.description == rhs.description &&
+               lhs.color == rhs.color
+    }
+}
