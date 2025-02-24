a781acf8f6fdf75adb054d6fe265b08a96d5c431
opfic <opficdev@gmail.com>
2025-02-24 22:01:48 +0900

    종일 일정을 표시하기 위한 ScrollView에 추가될 패딩 추가

--- SnapPlan/ViewModel/UIViewModel.swift
+++ SnapPlan/ViewModel/UIViewModel.swift
@@ -8,5 +8,6 @@
 import SwiftUI
 
 class UIViewModel: ObservableObject {
-    @Published var bottomPadding = CGFloat.zero
+    @Published var allDayPadding = CGFloat.zero    //  종일 이벤트를 보여주는 뷰에 의해 가려지는 만큼 ScrollView 내부에 추가되는 패딩
+    @Published var sheetPadding = CGFloat.zero //  sheet에 의해 가려지는 만큼 ScrollView 내부에 추가되는 패딩
 }
