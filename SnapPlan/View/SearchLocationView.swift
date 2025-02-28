64b6804718f7584c7aee22bc62e6981754f4a070
opfic <opficdev@gmail.com>
2025-02-28 21:24:09 +0900

    MVVM 패턴 적용 및 ui 수정

--- SnapPlan/View/SearchLocationView.swift
+++ SnapPlan/View/SearchLocationView.swift
@@ -9,7 +9,9 @@ import SwiftUI
 
 struct SearchLocationView: View {
     @EnvironmentObject var scheduleVM: ScheduleViewModel
-    @State private var location: String = ""
+    @EnvironmentObject var searchVM: SearchLocationViewModel
+    @Environment(\.dismiss) var dismiss
+    @Environment(\.colorScheme) var colorScheme
     @FocusState private var focused: Bool
     
     var body: some View {
@@ -17,23 +19,47 @@ struct SearchLocationView: View {
             HStack {
                 Image(systemName: "magnifyingglass")
                     .foregroundColor(.gray)
-                TextField("위치", text: $location)
+                TextField("위치", text: $searchVM.query)
                     .focused($focused)
                     .onAppear {
                         focused = true
                     }
+                if !searchVM.query.isEmpty {
+                    Button(action: {
+                        scheduleVM.location.removeAll()
+                        searchVM.query.removeAll()
+                        searchVM.suggestions.removeAll()
+                    }) {
+                        Image(systemName: "xmark.circle.fill")
+                            .foregroundColor(.gray)
+                    }
+                }
             }
             .padding(8)
             .background(
                 RoundedRectangle(cornerRadius: 10)
-                    .fill(Color(.systemGray6))
+                    .fill(Color(colorScheme == .light ? .systemGray5 : .systemGray4))
             )
+            .padding(.horizontal)
+            
+            List(searchVM.suggestions, id: \.self) { suggestion in
+                Text(suggestion.title)
+                    .onTapGesture {
+                        scheduleVM.location = suggestion.title
+                        searchVM.query = suggestion.title
+                        dismiss()
+                    }
+            }
         }
+        .ignoresSafeArea(.container, edges: [.horizontal, .bottom])
         .frame(maxHeight: .infinity, alignment: .top)
-        .padding(.horizontal)
-        .ignoresSafeArea(.all, edges: [.horizontal, .bottom])
         .onDisappear {
-            scheduleVM.location = location
+            if searchVM.suggestions.isEmpty {
+                scheduleVM.location = searchVM.query
+            }
+            DispatchQueue.main.async {
+                focused = false
+            }
         }
     }
 }
@@ -41,6 +67,7 @@ struct SearchLocationView: View {
 #Preview {
     SearchLocationView()
         .environmentObject(ScheduleViewModel())
+        .environmentObject(SearchLocationViewModel())
 }
 
 
