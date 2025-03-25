ec8c02cb7574b0513e2c89cf81f05130c1f196bd
opfic <opficdev@gmail.com>
2025-03-25 18:15:51 +0900

    refactor: ui 수정

--- SnapPlan/View/Schedules/VoiceMemoView.swift
+++ SnapPlan/View/Schedules/VoiceMemoView.swift
@@ -9,29 +9,52 @@ import SwiftUI
 
 struct VoiceMemoView: View {
     @EnvironmentObject var scheduleVM: ScheduleViewModel
+    @Environment(\.dismiss) var dismiss
     let circleRadius = UIScreen.main.bounds.width * 0.12
     
     var body: some View {
-        ZStack {
-            Color.timeLine.ignoresSafeArea()
-            VStack {
-                ZStack {
-                    Circle()
-                        .stroke(Color.gray, lineWidth: 3)
-                        .frame(width: circleRadius)
-                    RoundedRectangle(cornerRadius: circleRadius * (scheduleVM.isRecording ? 0.15 : 1))
-                        .fill(Color.blue)
-                        .frame(width: circleRadius * (scheduleVM.isRecording ? 0.5 : 0.8), height: circleRadius * (scheduleVM.isRecording ? 0.5 : 0.8)
-                        )
-                }
-                .onTapGesture {
-                    withAnimation {
-                        if scheduleVM.isRecording {
-//                            scheduleVM.stopRecord()
-                        }
-                        else {
-//                            scheduleVM.startRecord()
-                        }
+        VStack(spacing: 100) {
+            Text("음성 메모")
+                .foregroundStyle(Color.gray)
+                .bold()
+            
+//                HStack(spacing: 2) {
+//                    ForEach(scheduleVM.audioLevels, id: \.self) { level in
+//                        Rectangle()
+//                            .fill(Color.timeBar)
+//                            .frame(width: 4, height: level)
+//                    }
+//                    .frame(height: UIScreen.main.bounds.height * 0.2)
+//                    .background(
+//                        GeometryReader { proxy in
+//                            Color.clear.onChange(of: proxy.size.width) { width in
+//                                if UIScreen.main.bounds.width <= width {
+//                                    scheduleVM.audioLevels.removeFirst()
+//                                }
+//                            }
+//                        }
+//                    )
+//                }
+//                .frame(maxWidth: .infinity, alignment: .trailing)
+        
+            
+            ZStack {
+                Circle()
+                    .stroke(Color.gray, lineWidth: 3)
+                    .frame(width: circleRadius)
+                RoundedRectangle(cornerRadius: circleRadius * (scheduleVM.isRecording ? 0.15 : 1))
+                    .fill(Color.timeBar)
+                    .frame(width: circleRadius * (scheduleVM.isRecording ? 0.5 : 0.8), height: circleRadius * (scheduleVM.isRecording ? 0.5 : 0.8)
+                    )
+            }
+            .onTapGesture {
+                withAnimation {
+                    if scheduleVM.isRecording {
+                        scheduleVM.stopRecord()
+                        dismiss()
+                    }
+                    else {
+                        scheduleVM.startRecord()
                     }
                 }
             }
