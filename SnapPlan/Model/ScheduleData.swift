9ac395968204ae76a2e12d1973438e0cdca75172
opfic <opficdev@gmail.com>
2025-03-28 15:32:51 +0900

    refactor: voiceMemo와 photos 변수 추가

--- SnapPlan/Model/ScheduleData.swift
+++ SnapPlan/Model/ScheduleData.swift
@@ -6,8 +6,9 @@
 //
 
 import Foundation
+import AVKit
 
-struct ScheduleData: Identifiable, Codable {
+struct ScheduleData: Identifiable {
     let id: UUID //  UUID
     var title: String   // 일정 제목
     var startDate: Date // 일정 시작 날짜
@@ -15,10 +16,12 @@ struct ScheduleData: Identifiable, Codable {
     var isChanging: Bool  // 일정 시간 변경 중인지 확인
     var isAllDay: Bool // 종일 일정 여부
     var cycleOption: CycleOption    // 일정 반복 주기
+    var color: Int  // 일정 색상(뷰에서 사용할 Color 배열의 인덱스임)
+    var voiceMemo: AVAudioFile?  // 음성 메모
+    var photos: [UIImage]    // 사진
     var location: String
     var address: String  // 장소에 대한 주소
     var description: String  // 일정 설명
-    var color: Int  // 일정 색상(뷰에서 사용할 Color 배열의 인덱스임)
     
     init(
         id: UUID = UUID(),
@@ -28,10 +31,12 @@ struct ScheduleData: Identifiable, Codable {
         isChanging: Bool = false,
         isAllDay: Bool = false,
         cycleOption: CycleOption = .none,
+        color: Int = 0,
+        voiceMemo: AVAudioFile? = nil,
+        photos: [UIImage] = [],
         location: String = "",
         address: String = "",
-        description: String = "",
-        color: Int = 0
+        description: String = ""
     ) {
         self.id = id
         self.title = title
@@ -40,20 +45,29 @@ struct ScheduleData: Identifiable, Codable {
         self.isChanging = isChanging
         self.isAllDay = isAllDay
         self.cycleOption = cycleOption
+        self.color = color
+        self.voiceMemo = voiceMemo
+        self.photos = photos
         self.location = location
         self.address = address
         self.description = description
-        self.color = color
     }
     
-    enum CycleOption: String, Codable {
-        case none = "none"  // 반복 없음
-        case daily = "daily"    // 매일
-        case weekdays = "weekdays"  // 평일
-        case weekly = "weekly"  // 매주
-        case biweekly = "biweekly"  // 2주마다
-        case monthly = "monthly"    // 매달
-        case yearly = "yearly"  // 매년
-        case custom = "custom"  // 사용자 정의
+    init(schedule: CodableScheduleData, voiceMemo: AVAudioFile? = nil, photos: [UIImage] = []) {
+        self.id = schedule.id
+        self.title = schedule.title
+        self.startDate = schedule.startDate
+        self.endDate = schedule.endDate
+        self.isChanging = schedule.isChanging
+        self.isAllDay = schedule.isAllDay
+        self.cycleOption = schedule.cycleOption
+        self.color = schedule.color
+        self.voiceMemo = voiceMemo
+        self.photos = photos
+        self.location = schedule.location
+        self.address = schedule.address
+        self.description = schedule.description
     }
+    
+
 }
