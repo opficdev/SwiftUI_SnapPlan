2e5240f3b311d74d8af8266e22fbfda12c4aae2d
opfic <opficdev@gmail.com>
2025-03-29 13:57:46 +0900

    feat: PhotosPicker에서 identifier과 UIImage를 가져오는 구조체

---
+++ SnapPlan/Model/ImageAsset.swift
@@ -0,0 +1,13 @@
+//
+//  ImageAsset.swift
+//  SnapPlan
+//
+//  Created by opfic on 3/29/25.
+//
+
+import SwiftUI
+
+struct ImageAsset: Identifiable, Equatable {
+    let id: String
+    let image: UIImage
+}
