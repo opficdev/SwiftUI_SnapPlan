8541fc7592b0f5f065ec3c243d2169bab96ae946
opfic <opficdev@gmail.com>
2025-03-31 23:30:05 +0900

    feat: Storage에서 받아오는 데이터들의 현재 상황을 저장하는 열거형

---
+++ SnapPlan/Enum/StorageState.swift
@@ -0,0 +1,15 @@
+//
+//  StorageState.swift
+//  SnapPlan
+//
+//  Created by opfic on 3/31/25.
+//
+
+import Foundation
+
+enum StorageState: Equatable {
+    case initial
+    case loading
+    case success
+    case error
+}
