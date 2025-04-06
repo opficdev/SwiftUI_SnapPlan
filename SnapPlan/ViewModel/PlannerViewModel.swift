b923a8fab4b6e1b108d10cb009ba47ce3d5a103c
opfic <opficdev@gmail.com>
2025-04-07 01:24:30 +0900

    fix: onAppear 이후 스크롤에 따른 날짜 변환이 되지 않던 현상 수정

--- SnapPlan/ViewModel/PlannerViewModel.swift
+++ SnapPlan/ViewModel/PlannerViewModel.swift
@@ -32,6 +32,10 @@ final class PlannerViewModel: ObservableObject {
                             self.selection = self.calendarData[1].firstIndex(
                                 where: { self.isSameDate(date1: $0, date2: self.today, components: [.year, .month, .day]) }
                             )!
+                            Task { @MainActor in
+                                try? await Task.sleep(nanoseconds: 1_000_000) // 0.001초 대기
+                                self.scrollTaskEnd = true
+                            }
                         }
                         else {
                             withAnimation(.easeInOut(duration: 0.2)) {
