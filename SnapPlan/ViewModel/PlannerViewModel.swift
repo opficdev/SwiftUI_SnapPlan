8d7b438754710752e80e9b706e149fd88d550f21
opfic <opficdev@gmail.com>
2025-04-13 15:52:22 +0900

    refactor: withAnimation 추가

--- SnapPlan/ViewModel/PlannerViewModel.swift
+++ SnapPlan/ViewModel/PlannerViewModel.swift
@@ -37,8 +37,17 @@ final class PlannerViewModel: ObservableObject {
             .removeDuplicates()
             .sink { [weak self] newValue in
                 guard let self = self else { return }
-                if self.calendarData.isEmpty || !self.isSameDate(date1: self.currentDate, date2: newValue, components: [.year, .month]) {
-                    self.setCalendarData(date: newValue)
+                
+                
+                if !self.isSameDate(date1: self.currentDate, date2: newValue, components: [.year, .month]) {
+                    if self.userTapped {
+                        withAnimation(.easeInOut(duration: 0.15)) {
+                            self.setCalendarData(date: newValue)
+                        }
+                    }
+                    else {
+                        
+                    }
                 }
                 self.wasPast = self.currentDate < newValue
                 self.currentDate = newValue
