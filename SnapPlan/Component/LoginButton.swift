89d574d50f0e1ded78d28be7f29cb744ba510a52
opfic <opficdev@gmail.com>
2025-04-25 20:31:28 +0900

    feat: 새로운 로그인 버튼 추가

---
+++ SnapPlan/Component/LoginButton.swift
@@ -0,0 +1,57 @@
+//
+//  LoginButton.swift
+//  SnapPlan
+//
+//  Created by opfic on 4/25/25.
+//
+
+import SwiftUI
+
+struct LoginButton: View {
+    @State private var logo: UIImage?
+    @State private var text = ""
+    @State private var height = CGFloat.zero
+    let action: () -> Void
+    
+    
+    init(logo: UIImage? = nil, text: String = "", action: @escaping () -> Void = {}) {
+        self._logo = State(initialValue: logo)
+        self._text = State(initialValue: text)
+        self.action = action
+    }
+    
+    var body: some View {
+        Button(action: {
+            action()
+        }) {
+            HStack {
+                Text(text)
+                    .foregroundStyle(Color.primary)
+                    .font(.system(size: height / 2))
+            }
+            .overlay {
+                if let logo = logo {
+                    Image(uiImage: logo)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: height / 2, height: height / 2)
+                }
+            }
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .overlay(
+            GeometryReader { proxy in
+                RoundedRectangle(cornerRadius: 5)
+                    .stroke(Color.gray, lineWidth: 2)
+                    .onAppear {
+                        height = proxy.size.height
+                    }
+            }
+        )
+        
+    }
+}
+
+#Preview {
+    LoginButton()
+}
