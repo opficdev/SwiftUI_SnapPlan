bd75026016d431421a4931b6e4db013a811b8177
opfic <opficdev@gmail.com>
2025-04-27 23:25:28 +0900

    refactor: UIImage -> Image 변경 및 로고 위치 변경

--- SnapPlan/Component/LoginButton.swift
+++ SnapPlan/Component/LoginButton.swift
@@ -8,13 +8,13 @@
 import SwiftUI
 
 struct LoginButton: View {
-    @State private var logo: UIImage?
+    @State private var logo: Image?
     @State private var text = ""
     @State private var height = CGFloat.zero
     let action: () -> Void
     
     
-    init(logo: UIImage? = nil, text: String = "", action: @escaping () -> Void = {}) {
+    init(logo: Image? = nil, text: String = "", action: @escaping () -> Void = {}) {
         self._logo = State(initialValue: logo)
         self._text = State(initialValue: text)
         self.action = action
@@ -29,23 +29,24 @@ struct LoginButton: View {
                     .foregroundStyle(Color.primary)
                     .font(.system(size: height / 2))
             }
-            .overlay {
-                if let logo = logo {
-                    Image(uiImage: logo)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: height / 2, height: height / 2)
-                }
-            }
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .overlay(
             GeometryReader { proxy in
-                RoundedRectangle(cornerRadius: 5)
-                    .stroke(Color.gray, lineWidth: 1)
-                    .onAppear {
-                        height = proxy.size.height
+                ZStack(alignment: .leading) {
+                    RoundedRectangle(cornerRadius: 5)
+                        .stroke(Color.gray, lineWidth: 1)
+                        .onAppear {
+                            height = proxy.size.height
+                        }
+                    if let logo = logo {
+                        logo
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: height / 2, height: height / 2)
+                            .padding(.leading)
                     }
+                }
             }
         )
         
